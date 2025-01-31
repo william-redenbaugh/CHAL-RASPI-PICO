934a2848fd2e7546935e7ffa334739f986b50910
William Redenbaugh <wmredenbaugh@gmail.com>
2025-01-31 05:36:55 -0800

    removing the wifi crap, moving to DMA based uart

--- chal_pico.cpp
+++ chal_pico.cpp
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include "btstack.h"
-#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
-#include "lwip/netif.h"
-#include "lwip/ip4_addr.h"
-#include "lwip/apps/lwiperf.h"
 
 #define AIRCR_Register (*((volatile uint32_t*)(PPB_BASE + 0x0ED0C)))
 
 bool initialize_pico(void){
-    if (cyw43_arch_init()) {
-        printf("failed to initialise cyw43_arch\n");
-        return false;
-    }
 
     return true;
 }
--- chal_uart.cpp
+++ chal_uart.cpp
@@ -3,13 +3,11 @@
 #include "hardware/uart.h"
 #include "hardware/irq.h"
 #include "CHAL_SHARED/os_uart.h"
+#include "dma_uart.hpp"
 
 typedef struct {
-    os_uart_t *uart;
-    unsafe_fifo_t rx_fifo_unsafe_fast;
-    safe_fifo_t rx_fifo_safe_slow;
-    os_setbits_t rx_trigger;
-
+    DmaUart *uart;
+    byte_array_fifo *rx_fifo_safe_slow;
 }int_uart_t;
 
 int_uart_t internal_uarts[2];
@@ -27,87 +25,30 @@ uart_inst_t *target_uart(int fd){
     }
 }
 
+static uint8_t uart_rx_buffer[320]; 
 void uart_rx_task(void *parameters){
     int_uart_t *uart = (int_uart_t*)parameters;
-    uart_inst_t *int_uart = target_uart(uart->uart->fd);
+    //uart_inst_t *int_uart = target_uart(uart->uart->fd);
 
     for(;;){
-        os_waitbits_indefinite(&uart->rx_trigger, BIT0);
-        
-        // Make some memory rq for coping over the data
-        int num_available_bytes = uart->rx_fifo_unsafe_fast.num_elements_in_queue;
-        uint8_t bytes[num_available_bytes];
-
-        // Quick lil transfer into a queue we can use more safely
-        unsafe_fifo_dequeue(&uart->rx_fifo_unsafe_fast, sizeof(uint8_t), bytes);
-        safe_fifo_enqueue(&uart->rx_fifo_safe_slow, num_available_bytes, bytes);
+        vTaskDelay(30);
+        int num_bytes = uart->uart->read_all(uart_rx_buffer);
+        if(num_bytes > 0){
+            enqueue_bytes_bytearray_fifo(uart->rx_fifo_safe_slow, uart_rx_buffer, num_bytes);
+        }    
     }    
 }
 
-void on_uart0_rx() {
-    while (uart_is_readable(uart0)) {
-        uint8_t ch = uart_getc(uart0);
-        // Can we send it back?
-        unsafe_fifo_enqueue(&internal_uarts[0].rx_fifo_unsafe_fast, 1, &ch);
-    }
-    os_setbits_signal(&internal_uarts[0].rx_trigger, BIT0);
-}
-
-void on_uart1_rx() {
-    while (uart_is_readable(uart1)) {
-        uint8_t ch = uart_getc(uart1);
-        // Can we send it back?
-        unsafe_fifo_enqueue(&internal_uarts[1].rx_fifo_unsafe_fast, 1, &ch);
-    }
-
-    os_setbits_signal(&internal_uarts[1].rx_trigger, BIT0);
-}
 
 int os_uart_begin(os_uart_t *uart, os_uart_config_t cfg, int fd, int baud){
     // Initialize UART 0 (default pins are GPIO0 for TX and GPIO1 for RX)
     
     uart_inst_t *int_uart = target_uart(fd);
-
-    // Set up our UART with the required speed.
-    uart_init(int_uart, baud);
-
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(cfg.tx_gpio, UART_FUNCSEL_NUM(int_uart, cfg.tx_gpio));
-    gpio_set_function(cfg.rx_gpio, UART_FUNCSEL_NUM(int_uart, cfg.rx_gpio));
-
-    uart->cfg = cfg;
-    uart->baud = baud; 
-    uart->fd = fd;
-
-    internal_uarts[fd].uart = uart;
-
-    // Small little queue used for any data coming straight from the RX handler
-    unsafe_fifo_queue_init(&internal_uarts[fd].rx_fifo_unsafe_fast, 64, sizeof(uint8_t));
-
+    internal_uarts[fd].uart = new DmaUart(int_uart, baud, cfg.rx_gpio, cfg.tx_gpio);
     // To be consumed and used by the OS at a later date
-    safe_fifo_init(&internal_uarts[fd].rx_fifo_safe_slow, 256, sizeof(uint8_t));
+    internal_uarts[fd].rx_fifo_safe_slow = create_byte_array_fifo(320);
 
-    // Letting thread know that we have new data
-    os_setbits_init(&internal_uarts[fd].rx_trigger);
-
-    // Need a thread to help with any incoming UART data
-    os_add_thread(uart_rx_task, &internal_uarts[fd], 1536, NULL);    
-
-    // Setting interrupt handler for incoming data on the RX line
-    int uart_irq = UART0_IRQ;
-    if(int_uart == uart0){
-        uart_irq = UART0_IRQ;
-        irq_set_exclusive_handler(uart_irq, on_uart0_rx);
-    }
-    else if(int_uart == uart1){
-        irq_set_exclusive_handler(uart_irq, on_uart1_rx);
-        uart_irq = UART1_IRQ;
-    }
-    irq_set_enabled(uart_irq, true);
-
-    // Now enable the UART to send interrupts - RX only
-    uart_set_irq_enables(int_uart, true, false);
+    os_add_thread(uart_rx_task, &internal_uarts[fd], 320, NULL);
 
     return OS_RET_OK;
 }
@@ -122,9 +63,6 @@ int os_uart_end(os_uart_t *uart){
         return OS_RET_NOT_INITIALIZED;
     }
 
-    uart_deinit(
-        target_uart(uart->fd)
-    );
 
     return OS_RET_OK;
 }
@@ -141,15 +79,10 @@ void os_uart_printf(os_uart_t *uart, const char *format, ...){
 
     sprintf(printf_out_arr, format);
 
-    uart_puts(target_uart(uart->fd), printf_out_arr);
 }
 
 int os_uart_readstring(os_uart_t *uart, uint8_t *data, size_t size){
-    return safe_fifo_dequeue_notimeout(
-        &internal_uarts[uart->fd].rx_fifo_safe_slow, 
-        size, 
-        data
-    );
+    return OS_RET_OK;
 }
 
 int os_uart_send(os_uart_t *uart, uint8_t *buf, size_t size){
@@ -173,18 +106,12 @@ int os_uart_send(os_uart_t *uart, uint8_t *buf, size_t size){
 }
 
 int os_uart_recieve(os_uart_t *uart, uint8_t *buf, size_t size){
-    return safe_fifo_dequeue_notimeout(
-        &internal_uarts[uart->fd].rx_fifo_safe_slow, 
-        size, 
-        buf
-    );
+    block_until_n_bytes_fifo(internal_uarts[uart->fd].rx_fifo_safe_slow, size);
+    return dequeue_bytes_bytearray_fifo(internal_uarts[uart->fd].rx_fifo_safe_slow, buf, size);
 }
 
 int os_uart_recieve_timeout(os_uart_t *uart, uint8_t *buf, size_t size, uint32_t timeout_ms){
-    return safe_fifo_dequeue_timeout(
-        &internal_uarts[uart->fd].rx_fifo_safe_slow, 
-        size, 
-        buf, 
-        timeout_ms
-    );
+    
+    block_until_n_bytes_fifo_timeout(internal_uarts[uart->fd].rx_fifo_safe_slow, size, timeout_ms);
+    return dequeue_bytes_bytearray_fifo(internal_uarts[uart->fd].rx_fifo_safe_slow, buf, size);
 }
